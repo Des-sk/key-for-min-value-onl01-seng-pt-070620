81422974a7040d8dd6e4c0ce6c53ac0ea138b597
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-18 00:58:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 
 def key_for_min_value(hash)
   lowest_key = nil 
-  lowest_value = 0 
+  lowest_value = nil 
   hash.each do |key, value|
     if lowest_value = 0 || value< lowest_value
       lowest_value= value
